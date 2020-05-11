465bd330daf65ab1282488f27a75489d37271a33
hawrot <mhawro200@caledonian.ac.uk>
2020-05-11 22:38:00 +0100

    Add styling for Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import Aux from '../../hoc/Aux';
 import React from "react";
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SlideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,4 @@
-import {Component} from "react";
+import React, {Component} from "react";
 import Aux from '../../hoc/Aux';
 
 class BurgerBuilder extends Component{
