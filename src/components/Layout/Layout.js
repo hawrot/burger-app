b585d575f17e6874f575b4548f13544dec625206
hawrot <mhawro200@caledonian.ac.uk>
2020-05-13 00:09:37 +0100

    Dismiss the sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,36 @@
 import Aux from '../../hoc/Aux';
-import React from "react";
+import React, {Component} from "react";
 import classes from './Layout.css';
 import Toolbar from '../navigation/toolbar/Toolbar';
 import SideDrawer from '../navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <div>
-            <Toolbar/>
-            <SideDrawer/>
-        </div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state ={
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    };
+
+    render(){
+        return(
+            <Aux>
+                <div>
+                    <Toolbar/>
+                    <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                </div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+
+};
+
+export default Layout;
 
 
 
