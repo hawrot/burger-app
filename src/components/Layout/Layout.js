89a573af66628f58356cb36de3dcb4a237d42f06
hawrot <mhawro200@caledonian.ac.uk>
2020-05-12 23:42:59 +0100

    Add working sidedrawer on small devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import Aux from '../../hoc/Aux';
 import React from "react";
 import classes from './Layout.css';
 import Toolbar from '../navigation/toolbar/Toolbar';
+import SideDrawer from '../navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <div>
             <Toolbar/>
+            <SideDrawer/>
         </div>
         <main className={classes.Content}>
             {props.children}
