cc179e0f1252145390433f15aa77abc01ba5d77c
hawrot <mhawro200@caledonian.ac.uk>
2020-05-12 23:07:32 +0100

    Generate basic logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import Aux from '../../hoc/Aux';
 import React from "react";
 import classes from './Layout.css';
+import Toolbar from '../navigation/toolbar/Toolbar';
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SlideDrawer, Backdrop</div>
+        <div>
+            <Toolbar/>
+        </div>
         <main className={classes.Content}>
             {props.children}
         </main>
