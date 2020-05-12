1ec9b961736ee18f128fbf827ab062c6987d3aa3
hawrot <mhawro200@caledonian.ac.uk>
2020-05-12 17:00:41 +0100

    Add styles to orderbutton

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -22,7 +22,7 @@ const buildControls = (props) => (
                 disabled={props.disabled[ctrl.type]}
             />
         ))}
-        <button>ORDER NOW</button>
+        <button className={classes.OrderButton}>ORDER NOW</button>
     </div>
 );
 
