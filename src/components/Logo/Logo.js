4f33c00a9b28ea9bc2a1721e3935227d9ba06ac1
hawrot <mhawro200@caledonian.ac.uk>
2020-05-12 23:17:25 +0100

    Add logo to the toolbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Logo.css';
+
+import burgerLogo from '../../assets/images/original.png';
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt=""/>
+    </div>
+);
+
+export default logo;
