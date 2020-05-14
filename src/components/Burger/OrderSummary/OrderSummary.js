2122b365414e7e5ae929260fe161d667639c6366
hawrot <mhawro200@caledonian.ac.uk>
2020-05-14 14:26:20 +0100

    Change Modal component from functional to class

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,17 +1,19 @@
-import React. {Component} from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
 
+    componentWillUpdate(){
 
+    }
 
     render() {
         const ingredientSummary = Object.keys(this.props.ingredients)
             .map(igKey => {
                 return (
                     <li key={igKey}>
-                        <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}
                     </li>
                 )
             })
@@ -20,13 +22,13 @@ class OrderSummary extends Component {
             <Aux>
                 <h3>Your Order:</h3>
                 <p>Burger with the following ingredients:</p>
-                <p>The price is £{props.totalPrice.toFixed(2)}</p>
+                <p>The price is £{this.props.totalPrice.toFixed(2)}</p>
                 <ul>
                     {ingredientSummary}
                 </ul>
                 <p>Go to checkout? </p>
-                <Button clicked={props.purchaseCancelled} btnType="Danger">Cancel</Button>
-                <Button clicked={props.purchaseContinued} btnType="Success">Continue</Button>
+                <Button clicked={this.props.purchaseCancelled} btnType="Danger">Cancel</Button>
+                <Button clicked={this.props.purchaseContinued} btnType="Success">Continue</Button>
             </Aux>
         );
     }
