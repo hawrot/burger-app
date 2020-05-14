69ed54ff390589aa7412e176f3c802bf270bc4a7
hawrot <mhawro200@caledonian.ac.uk>
2020-05-14 14:03:11 +0100

    Display sideDrawer on button click

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -14,11 +14,15 @@ class Layout extends Component {
         this.setState({showSideDrawer: false})
     };
 
+    sideDrawerToggleHandler =() =>{
+        this.setState((prevState) =>{ return {showSideDrawer: !prevState.showSideDrawer}})
+    };
+
     render(){
         return(
             <Aux>
                 <div>
-                    <Toolbar/>
+                    <Toolbar drawerToogleClicked={this.sideDrawerToggleHandler}/>
                     <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
                 </div>
                 <main className={classes.Content}>
