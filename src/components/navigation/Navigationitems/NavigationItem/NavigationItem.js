01fbeb11456ee9ade9cdd5808bf358deeba27219
hawrot <mhawro200@caledonian.ac.uk>
2020-05-16 23:51:27 +0100

    Add routing using NavLink

--- src/components/navigation/Navigationitems/NavigationItem/NavigationItem.js
+++ src/components/navigation/Navigationitems/NavigationItem/NavigationItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import classes from './NavigationItem.css';
+import {NavLink} from 'react-router-dom';
 
 const navigationItem = (props) => (
     <ul className={classes.NavigationItem}>
-        <li><a href={props.link} className={props.active ? classes.active : null}> {props.children}</a> </li>
+        <li>
+            <NavLink to={props.link} activeClassName={classes.active}> {props.children}</NavLink>
+        </li>
     </ul>
 );
 
