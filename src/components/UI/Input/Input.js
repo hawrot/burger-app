9d057fa827c77b876b896c589200d54c74804f1d
hawrot <mhawro200@caledonian.ac.uk>
2020-05-17 00:39:14 +0100

    Add switch statement to handle different input type

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const input = (props) => {
+
+    let inputElement = null;
+
+    switch (props.inputType) {
+        case ('input'):
+            inputElement = <input/>
+        case('textarea'):
+            inputElement = <textarea/>
+    }
+
+    return (
+        <div>
+            <label>{props.label}</label>
+            <input type="text"/>
+        </div>
+    )
+}
+
+
+export default input;
