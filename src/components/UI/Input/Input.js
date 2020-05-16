3754e9531fdf4a1853a7852a7679a5f0c9533bfd
hawrot <mhawro200@caledonian.ac.uk>
2020-05-17 00:43:42 +0100

    Add styling to Input component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,15 +6,19 @@ const input = (props) => {
 
     switch (props.inputType) {
         case ('input'):
-            inputElement = <input/>
+            inputElement = <input{...props}/>
+            break;
         case('textarea'):
-            inputElement = <textarea/>
+            inputElement = <textarea {...props}/>
+            break;
+        default:
+            inputElement = <input {...props}/>
     }
 
     return (
         <div>
             <label>{props.label}</label>
-            <input type="text"/>
+            {inputElement}
         </div>
     )
 }
