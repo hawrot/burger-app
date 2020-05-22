e2bad4984ccb800623e213275812546f399bad8a
hawrot <mhawro200@caledonian.ac.uk>
2020-05-23 00:07:30 +0100

    Display the error messages

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -122,8 +122,16 @@ class Auth extends Component {
             form = <Spinner/>
         }
 
+        let errorMessage = null;
+        if(this.props.error){
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            )
+        }
+
         return (
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
@@ -136,7 +144,8 @@ class Auth extends Component {
 
 const mapStateToProps = state =>{
     return{
-        loading: state.auth.loading
+        loading: state.auth.loading,
+        error: state.auth.error
     }
 }
 
