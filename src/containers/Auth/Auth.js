ba6e4874bbfbcb57568fa856fe4148aa434fdd39
hawrot <mhawro200@caledonian.ac.uk>
2020-05-22 17:27:08 +0100

    Add styling for auth component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
-import * as actions from '../../store/actions/index';
+
 
 class Auth extends Component {
     state = {
@@ -121,10 +121,6 @@ class Auth extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
-    };
-};
 
-export default connect(null, mapDispatchToProps)(Auth);
+
+export default Auth;
