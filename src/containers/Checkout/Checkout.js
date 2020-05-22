5029909c8a4b6e5c248d5e33a3382506e2bf614e
hawrot <mhawro200@caledonian.ac.uk>
2020-05-22 13:41:34 +0100

    Refactor the reducers to minimise the code

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,28 +1,24 @@
-import React, {Component} from 'react';
-import {Route, Redirect} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './Contact/Contact';
-import {connect} from 'react-redux';
 import * as actions from '../../store/actions/index';
 
-
 class Checkout extends Component {
-
-
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
 
     checkoutContinuedHandler = () => {
-        this.props.history.replace('/checkout/contact-data');
+        this.props.history.replace( '/checkout/contact-data' );
     }
 
-    render() {
-        let summary = <Redirect to="/"/>
-
-        if (this.props.ings) {
+    render () {
+        let summary = <Redirect to="/" />
+        if ( this.props.ings ) {
             const purchasedRedirect = this.props.purchased ? <Redirect to="/"/> : null;
             summary = (
                 <div>
@@ -30,14 +26,12 @@ class Checkout extends Component {
                     <CheckoutSummary
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
-                        checkoutContinued={this.checkoutContinuedHandler}
-                    />
+                        checkoutContinued={this.checkoutContinuedHandler} />
                     <Route
                         path={this.props.match.path + '/contact-data'}
-                        component={ContactData}/>
-
+                        component={ContactData} />
                 </div>
-            )
+            );
         }
         return summary;
     }
@@ -46,11 +40,8 @@ class Checkout extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
-        purchased: state.order.purchasing
-
+        purchased: state.order.purchased
     }
 };
 
-
-
-export default connect(mapStateToProps)(Checkout);
+export default connect( mapStateToProps )( Checkout );
