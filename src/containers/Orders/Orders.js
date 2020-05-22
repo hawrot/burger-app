089a952d1891b55d5fbc8f3daeaeabcff5b89e23
hawrot <mhawro200@caledonian.ac.uk>
2020-05-22 13:20:55 +0100

    Fetch the orders with redux and handle the spinner once loading

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+import * as actions from '../../store/actions/index';
+import {connect} from 'react-redux';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
 
@@ -11,19 +14,37 @@ class Orders extends Component {
     }
 
     componentDidMount() {
-
+        this.props.onFetchOrders();
     }
 
     render() {
+        let orders = <Spinner/>
+        if (!this.props.loading) {
+            orders = this.props.orders.map(order => (
+                    <Order price={+order.price} ingredients={order.ingredients} key={order.id}/>
+                )
+            )
+        }
         return (
             <div>
-                {this.state.orders.map(order =>(
-                    <Order price={+order.price} ingredients={order.ingredients} key={order.id}/>
-                ))}
+                {orders}
             </div>
         );
 
     }
 }
 
-export default withErrorHandler(Orders, axios);
+const mapStateToProps = state => {
+    return {
+        orders: state.order.orders,
+        loading: state.order.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: () => dispatch(actions.fetchOrders())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
